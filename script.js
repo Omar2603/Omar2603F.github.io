6f69e001b1f4b335dd201268107fe98d9302a447
Omar2603 <157431870+Omar2603@users.noreply.github.com>
2024-03-18 14:15:56 -0400

    P1. Work-in-progress report

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-// When the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
-    // Get all nav links
+
     var navLinks = document.querySelectorAll('.navbar a');
   
     document.getElementById('scroll-to-about').addEventListener('click', function() {
         document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
     });
     
-    // Function to remove active class from all links
     function removeActiveClasses() {
         navLinks.forEach(function (link) {
             link.classList.remove('active');
         });
     }
-  
-    // Add click event to all nav links
+
     navLinks.forEach(function (link) {
         link.addEventListener('click', function () {
             removeActiveClasses();
-            this.classList.add('active'); // Add active class to the clicked link
+            this.classList.add('active');
         });
     });
 
-    // Add mouseenter event to each section to highlight the corresponding nav link
     document.querySelectorAll('section').forEach(function (section) {
         section.addEventListener('mouseenter', function () {
             removeActiveClasses();
@@ -32,7 +28,6 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
 
-        // Add mouseleave event to each section to remove the highlight from the nav link
         section.addEventListener('mouseleave', function () {
             removeActiveClasses();
         });
